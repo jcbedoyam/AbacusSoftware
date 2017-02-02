32c782abd9470ed99e507cb5c990b7f9ea20e5be
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-02-02 08:57:08 -0500

    more core

--- pi.py
+++ pi.py
@@ -10,19 +10,19 @@ while 1:
     try:
         out = ser.readline()
         if out != '':
-            print ">>> %s"%out
-        if out[:3] == 'do:':
-            command = out[4:]
-            text = "Done"
-            try:
-                exec(command)
-            except Exception as e:
-                out = e
-                print "Error: %s is not valid"%command
-                text = "Error: %s"%out
-            sendmessage(ser, text)
-        else:
-            sendmessage(ser, "Ok")
+            print(">>> %s"%out)
+            if out[:3] == 'do:':
+                command = out[4:]
+                text = "Done"
+                try:
+                    exec(command)
+                except Exception as e:
+                    out = e
+                    print("Error: %s is not valid"%command)
+                    text = "Error: %s"%out
+                sendmessage(ser, text)
+            else:
+                sendmessage(ser, "Ok")
         time.sleep(0.01)
     except KeyboardInterrupt:
         ser.close()
