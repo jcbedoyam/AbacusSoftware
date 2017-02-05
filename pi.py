cc900fae1f792ac7e3b98effc368c557fad6ab71
jsbarbosa <js.barbosa10@uniandes.edu.co>
2017-02-05 01:55:22 -0500

    pi

--- pi.py
+++ pi.py
@@ -1,4 +1,5 @@
 from core import *
+import numpy as np
 
 ser = serialPort('/dev/ttyAMA0')
 
@@ -6,21 +7,25 @@ print("Waiting...")
 
 while 1:
     try:
-        out = ser.readline().decode()[:-1]
+        out = ser.serial.read(6).encode('hex')
+#        output = ' '.join(map(lambda x:x.encode('hex'), out))
         if out != '':
+            out = map(''.join, zip(*[iter(out)]*2))
             print(">>> %s"%out)
-            if out[:3] == 'do:':
-                command = out[4:]
-                text = "Done"
-                try:
-                    exec(command)
-                except Exception as e:
-                    out = e
-                    print("Error: %s is not valid"%command)
-                    text = "Error: %s"%out
-                sendmessage(ser, text)
-            else:
-                sendmessage(ser, "Ok")
+            if int(out[1], 16) == 0x0E:
+                address = int(out[2], 16)
+                msb = int(out[3], 16)
+                lsb = int(out[4], 16)
+                if  msb + lsb == 0:
+                    stop = max(ADDRESS.values())
+                    n_bytes = 5*(stop-address)
+                    send = [n_bytes]
+                    for i in range(address, stop+1):
+                        value = np.random.randint(200e6)
+                        print("%d %d"%(i, value))
+                        value = "%08X"%value
+                        send += [i] + [int(value[2*j:2*j+2], 16) for j in range(4)]
+                    ser.message(send, read=True)
         time.sleep(0.01)
     except KeyboardInterrupt:
         ser.close()
