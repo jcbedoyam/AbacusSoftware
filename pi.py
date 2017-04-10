7193be51b3c2039ce9ee47b097e663b7e76957c0
jsbarbosa <js.barbosa10@uniandes.edu.co>
2017-04-10 16:52:01 -0500

    pi included

--- pi.py
+++ pi.py
@@ -8,7 +8,7 @@ print("Waiting...")
 while 1:
     try:
         out = ser.serial.read(6).encode('hex')
-#        output = ' '.join(map(lambda x:x.encode('hex'), out))
+        output = ' '.join(map(lambda x:x.encode('hex'), out))
         if out != '':
             out = map(''.join, zip(*[iter(out)]*2))
             print(">>> %s"%out)
@@ -16,19 +16,20 @@ while 1:
                 address = int(out[2], 16)
                 msb = int(out[3], 16)
                 lsb = int(out[4], 16)
-                if  msb + lsb == 0:
+                if  msb + lsb == 6:
                     stop = max(ADDRESS.values())
                     n_bytes = 3*(stop-address)
                     send = [n_bytes]
                     
                     items = int((stop-address)/2)
-                    for i in range(items):
-                        value = 6*np.random.random()
-                        value = int(10**value)
+                    for i in range(items + 1):
+#                        value = 6*np.random.random()
+#                        value = int(10**value)
+                        value = np.random.randint(100)
                         
                         value = "%08X"%value
-                        send += [2*i + address] + [int(value[2*j:2*j+2], 16) for j in range(2)]
-                        send += [2*i+1 + address] + [int(value[2*j:2*j+2], 16) for j in range(2, 4)]
+                        send += [2*i + address] + [int(value[2*j:2*j+2], 16) for j in range(2,4)]
+                        send += [2*i+1 + address] + [int(value[2*j:2*j+2], 16) for j in range(2)]
                     print(send)
                     ser.message(send, read=True)
         sleep(0.00001)
