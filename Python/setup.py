b8bd6af618c4e3cc5ba45eb5ec707fc5a859db9b
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-05-05 18:06:25 -0500

    other way

--- Python/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name = 'reimaginedQuantum',
-       version = '1.0',
-       description = 'Reimagined Quantum attemp',
-       packages = ['reimaginedQuantum'])
