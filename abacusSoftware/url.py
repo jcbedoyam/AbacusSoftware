32f1e76c54b204dab3dd7a4ff3c6c5be00ed96fc
David Guzmán <da.guzman@outlook.com>
2020-06-30 11:36:01 -0500

    to version 1.4.0

--- abacusSoftware/url.py
+++ abacusSoftware/url.py
@@ -20,9 +20,11 @@ def checkUpdate():
 
     current_version = versionstr(constants.__version__)
     n = min(len(url_version), len(current_version))
-
+    
     for i in range(n):
         if url_version[i] > current_version[i]:
             return ".".join([str(v) for v in url_version])
+        elif url_version[i] < current_version[i]: #corrected on v1.4.0 (2020-06-29)
+            return None  #if current_version is greater than url_version, do not update
 
     return None
